999b01e54a168340b0f82f249d71245af3547d45
Erkhem Narangerel <erkhem.narangerel@redknee.com>
2016-04-13 00:09:13 +0200

    new test data created with different sizes of jobs

--- src/ChooseAndRunAlgorithm.java
+++ src/ChooseAndRunAlgorithm.java
@@ -6,7 +6,9 @@ import java.util.Collections;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.google.common.base.Stopwatch;
 import com.google.common.collect.Range;
 import com.google.common.collect.RangeMap;
 import com.google.common.collect.TreeRangeMap;
@@ -21,7 +23,9 @@ public class ChooseAndRunAlgorithm {
 
 		static LateJobP1 alg1 = new LateJobP1();
 		static ObjectMapper mapper = new ObjectMapper();
-		private static String fileDirectory = "data/json_small_test.json";
+		private static String fileDirectory = "data/json_300_test.json";
+		//to measure execution time
+		Stopwatch stopwatch = Stopwatch.createUnstarted();
 		
 	public static void main(String[] args) {		
 		
@@ -32,23 +36,28 @@ public class ChooseAndRunAlgorithm {
 			//To write to file JSON change the name and directory.
 			/*
 			ArrayList<Job> jobs12 = new ArrayList<Job>();
-			for(int i = 1; i<=5; i++){
-				jobs12.add(new Job(5));
+			for(int i = 1; i<=10; i++){
+				jobs12.add(new Job(10));
 			}
-			mapper.writeValue(new File("json_small.json"),jobs12);
+			mapper.enable(SerializationFeature.INDENT_OUTPUT);
+			mapper.writeValue(new File(fileDirectory),jobs12);
 			*/
 			
 			ArrayList<Job> jobs = mapper.readValue(new File(fileDirectory), new TypeReference<ArrayList<Job>>() {
 			});
-			ArrayList<Job> copyOfOriginal = jobs;
+			ArrayList<Job> copyOfOriginal = new ArrayList<>(jobs);
+			
 			Exhaustive exhaustive = new Exhaustive();
-			ArrayList<Job> emptyList = new ArrayList<Job>();
+			
+			/*//Exhaustive search
 			exhaustive.permutation(jobs, jobs.size());  // (jobs, emptyList);
 			System.out.println(exhaustive.getMinGlobal());
 			
+			*/
+			
 			//Greedy Algorithm with only sorting wieght
 			greedyA.sortWeights(jobs);
-			System.out.println("Greedy Algorithm solution is "+exhaustive.calculate(jobs));
+			System.out.println("No disturbance Greedy Algorithm solution is "+exhaustive.calculate(jobs));
 			
 			
 			//Greedy Algorithm with time interval and universal
